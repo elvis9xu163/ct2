14c3971a426e510dedc2f082e07349f7301474c3
elvis <elvis9xu@163.com>
2015-05-03 01:01:30 +0800

    ct2->code->dev->ing: 调整可用

--- ct-web/src/main/java/com/xjd/ct/web/ctrlr/v10/ResourceController10.java
+++ ct-web/src/main/java/com/xjd/ct/web/ctrlr/v10/ResourceController10.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.xjd.ct.biz.bo.ResourceBo;
@@ -45,6 +46,8 @@ public class ResourceController10 {
 	@Autowired
 	UserService userService;
 
+	@RequestMapping("/upload")
+	@ResponseBody
 	public View upload(@RequestParam(value = "timestamp", required = false) String timestamp,
 			@RequestParam(value = "token", required = false) String token,
 			@RequestParam(value = "sign", required = false) String sign,
