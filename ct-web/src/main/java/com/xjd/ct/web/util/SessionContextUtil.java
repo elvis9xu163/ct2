50dcb714d4d09e97550ba83730b3e6a7da4f46b3
elvis <elvis9xu@163.com>
2015-05-27 18:16:51 +0800

    ct2->code-dev->done: 冲突合并问题解决

--- ct-web/src/main/java/com/xjd/ct/web/util/SessionContextUtil.java
+++ ct-web/src/main/java/com/xjd/ct/web/util/SessionContextUtil.java
@@ -4,34 +4,26 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.xjd.ct.biz.bo.TokenBo;
-<<<<<<< HEAD
 import com.xjd.ct.biz.bo.UserBo;
-=======
->>>>>>> origin/master
 
 public class SessionContextUtil {
 
 	public static final String TOKEN_KEY = SessionContextUtil.class.getSimpleName() + "token";
-<<<<<<< HEAD
 	public static final String USER_KEY = SessionContextUtil.class.getSimpleName() + "user";
-=======
->>>>>>> origin/master
 
-	public static void pubToken(TokenBo token) {
+	public static void putToken(TokenBo token) {
 		((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession().setAttribute(TOKEN_KEY, token);
 	}
 
 	public static TokenBo getToken() {
 		return (TokenBo) ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession().getAttribute(TOKEN_KEY);
 	}
-<<<<<<< HEAD
-	public static void pubUser(UserBo user) {
+
+	public static void putUser(UserBo user) {
 		((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession().setAttribute(USER_KEY, user);
 	}
 
 	public static UserBo getUser() {
 		return (UserBo) ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession().getAttribute(USER_KEY);
 	}
-=======
->>>>>>> origin/master
 }
